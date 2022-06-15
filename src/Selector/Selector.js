5c826e46cfadde6c3c6b4607090dc96ad759854f
Axent13 <Axent13@yandex.ru>
2022-06-15 11:28:28 +0700

    Верстка Компонента

--- src/Selector/Selector.js
+++ src/Selector/Selector.js
@@ -1,9 +1,11 @@
+import './Selector.scss';
+
 class Selector {
   constructor(options) {
     if (options === undefined) {
       options = ['1', '2', '3'];
     }
-    
+
     this.options = options;
     console.log('Selector succesfully created!');
     console.log(this.options);
@@ -13,6 +15,28 @@ class Selector {
     this.$selectorElement = document.createElement('div');
     this.$selectorElement.classList.add('selector');
 
+    this.$selectedValueElement = document.createElement('div');
+    this.$selectedValueElement.classList.add('selector__selected-value');
+    this.selectedValue = this.options[0];
+    this.$selectedValueElement.innerText = this.selectedValue;
+
+    this.$arrowElement = document.createElement('div');
+    this.$arrowElement.classList.add('selector__arrow');
+    this.$selectedValueElement.appendChild(this.$arrowElement);
+
+    this.$optionsContainerElement = document.createElement('div');
+    this.$optionsContainerElement.classList.add('selector__options-container');
+
+    this.options.forEach(element => {
+      this.$optionElement = document.createElement('div');
+      this.$optionElement.classList.add('selector__option');
+      this.$optionElement.innerText = element;
+      this.$optionsContainerElement.appendChild(this.$optionElement);
+    });
+    
+    this.$selectorElement.appendChild(this.$selectedValueElement);
+
+    this.$selectorElement.appendChild(this.$optionsContainerElement);
     $rootElement.appendChild(this.$selectorElement);
   }
 }
